8c9c68cc3a62f77381ee08128088b6a1de20df58
nicoripa <54992361+nicoripa@users.noreply.github.com>
2020-04-27 21:23:48 +0200

    kk

--- R/script.R
+++ R/script.R
@@ -1,18 +1,23 @@
 library(Matrix)
-library(e1071)
-
-A = readMM("/Users/nicoloripamonti/Desktop/universit??/Magistrale/Secondo Semestre/Metodi del Calcolo Scientifico/Progetto/MCS_Project/Matrici/ex15.mtx")
+install.packages("matlib")
+noA = readMM("/Users/nicoloripamonti/Desktop/universit??/Magistrale/Secondo Semestre/Metodi del Calcolo Scientifico/Progetto/MCS_Project/Matrici/ex15.mtx")
 
 A = t(A)*A
 
-sol = rep(1, times = nrow(A))
+xe = matrix(rep(1, times = nrow(A)), nrow = nrow(A), ncol = 1)
 
-b = A * sol
+b = (A %*% xe)
 
 C = chol(A)
 
 y1 = t(C) / b
 
-x1 = C / y1
+x = C / y1
 
-Err.ass = norm(x1 - sol)
+ErrRel = norm(x - xe)
+
+ErrAss = ErrRel / xe
+
+R = sqrt(3) * sqrt(3) - 3
+
+
