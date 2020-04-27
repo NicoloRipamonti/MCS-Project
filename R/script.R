58435fce8e5bdaac0e5dbd835c20aa3d3270d497
nicoripa <54992361+nicoripa@users.noreply.github.com>
2020-04-27 17:37:08 +0200

    kk

--- R/script.R
+++ R/script.R
@@ -1,9 +1,19 @@
 library(Matrix)
 library(e1071)
-library(bigmemory)
 
-A = readMM('../Matrici/ex15.mtx')
+A = readMM("/Users/nicoloripamonti/Desktop/universit??/Magistrale/Secondo Semestre/Metodi del Calcolo Scientifico/Progetto/MCS_Project/Matrici/ex15.mtx", 
+           package = "Matrix")
 
-start_time <- Sys.time()
-C = Cholesky(A)
-print(Sys.time() - start_time)
+A = t(A)*A
+
+sol = rep(1, times = nrow(A))
+
+b = A * sol
+
+C = chol(A)
+
+y1 = t(C) / b
+
+x1 = C / y1
+
+Err.ass = norm(x1 - sol)
