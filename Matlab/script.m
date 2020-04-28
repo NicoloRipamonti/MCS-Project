d7780985b1803dadcf9ba515f50f82180dd85af9
narger-ef <lorenzo.rovida@outlook.it>
2020-04-28 12:43:06 +0200

    matlab

---
+++ Matlab/script.m
@@ -0,0 +1,30 @@
+%Leggo la matrice A
+A = Problem.A;
+
+%Creo soluzione che è un vettore lungo righe di A con tutti 1
+xe = ones(size(A,1), 1);
+
+%Calcolo b dalla equazione Axe = b
+b = A * xe;
+
+%Calcolo Cholsky triangolare inferiore della matrice A 
+R = chol(A);
+
+%{
+Calcolo la soluzione del sistema:
+Siccome per ipotesi di Cholesky A = R * R',
+allora risolvere Ax = b diventa
+R * R' * x = b,
+quindi x = R \ (R' \ b)
+(N.B. A\B è l'operatore che risolve Ax=B)
+%}
+
+y = R' \ b;
+x = R \ y;
+
+%{
+Calcolo errore fra x (soluzione calcolata) e xe (soluzione
+con tutti 1)
+%}
+
+errore = norm(x - xe) / norm(xe);
