64898960a717deebfa5fd4a2ccc4ab56aae3fcec
nicoripa <54992361+nicoripa@users.noreply.github.com>
2020-05-09 16:51:09 +0200

    kkk

--- R/script.R
+++ R/script.R
@@ -1,14 +1,19 @@
 library(Matrix)
 
 start_time_setup <- Sys.time()
+
+#Leggo la matrice A
 A = readMM("../Matrici/ex15.mtx")
 
+#Creo soluzione che e' un vettore lungo righe di A con tutti 1
 xe = rep(1, times = nrow(A))
 
+#Calcolo b dalla equazione A * xe = b
 b = A %*% xe
 end_time_setup <- Sys.time()
 
 start_time <- Sys.time()
+#Calcolo Cholesky triangolare inferiore della matrice A
 R = chol(A)
 end_time <- Sys.time()
 
@@ -22,10 +27,12 @@ end_time_solving <- Sys.time()
 
 xe = as.matrix(xe)
 
+#Calcolo errore tra x e xe
 Errore = norm(x - xe) / norm(as.matrix(xe))
 
 format(Errore, scientific = TRUE)
 
+#Calcolo dei vari tempi
 Time_Setup = end_time_setup - start_time_setup
 Time_Chol  = end_time - start_time
 Time_Solving = end_time_solving - start_time_solving
