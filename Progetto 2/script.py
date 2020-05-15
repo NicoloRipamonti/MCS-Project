2d4f6c089cc0a15a0c108bd0a710ed4ba82a6fc9
narger-ef <lorenzo.rovida@outlook.it>
2020-05-15 15:50:45 +0200

    script

--- Progetto 2/script.py
+++ Progetto 2/script.py
@@ -1,2 +1,52 @@
-import tkinter as tk
+from tkinter import * 
+from tkinter.ttk import *
+from tkinter.filedialog import askopenfile 
+from tkinter.messagebox import showerror
+from PIL import Image
+import numpy as np
+
+file = ""
+
+def main_function() :
+    f = int(spin_F.get())
+    d = int(spin_d.get())
+
+    if d < 0 or d > 2 * f - 2:
+        showerror("Errore", "d dev'essere compresa fra 0 e 2F-2")
+
+    im = Image.open(file.name)
+    p = np.array(im)
+
+    
+
+def open_file(): 
+    file = askopenfile(mode ='r', filetypes =[('Immagine bmp - toni di grigio', '*.bmp')]) 
+
+
+root = Tk() 
+root.geometry('300x200') 
+
+w = Label(root, text="\nBenvento nel programma di Lorenzo e Nicolò!\nQua avverranno le magie", justify=CENTER)
+w.pack()
+
+btn = Button(root, text ='Open', command = lambda:open_file()) 
+
+btn.pack(side = TOP, pady = 10) 
+
+spin_F = Spinbox(root, from_=0, to=100, width=5)
+
+spin_F.pack()
+
+spin_d = Spinbox(root, from_=0, to=100, width=5)
+
+spin_d.pack()
+
+btn = Button(root, text ='Avvia', command = lambda:main_function())
+
+btn.pack(side = TOP, pady = 10) 
+
+mainloop() 
+
+
+
 
