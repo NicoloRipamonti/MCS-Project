cc13563cda0c66069a2d3f903f95b1cee4c10912
narger-ef <lorenzo.rovida@outlook.it>
2020-05-20 13:53:02 +0200

    ciao

--- Progetto 2/script2.py
+++ Progetto 2/script2.py
@@ -8,7 +8,6 @@ import numpy as np
 import os
 import cv2
 from scipy.fftpack import dct, idct
-
 import matplotlib.pylab as plt
 from matplotlib.backends.backend_tkagg import (
     FigureCanvasTkAgg, NavigationToolbar2Tk)
@@ -16,21 +15,11 @@ from matplotlib.backends.backend_tkagg import (
 from matplotlib.backend_bases import key_press_handler
 from matplotlib.figure import Figure
 
-
-file_path = ""
-img = []
-
-lista_blocchi = []
-lista_blocchi_inversa = []
-img_compressa = []
-colonne = []
-
 def suddividi(img, f):
-    global lista_blocchi
-
     i = 0
     j = 0
-    
+    lista_blocchi = []
+
     while (i + f < img.shape[0]):
         while(j + f < img.shape[1]):
             blocco = img[i:i+f,j:j+f]
@@ -39,53 +28,57 @@ def suddividi(img, f):
         i = i + f
         j = 0
 
-    return img
+    return lista_blocchi
 
-def applica_dct(img, d, f_dim):
-    global lista_blocchi_inversa
-    global img_compressa
-    
+def applica_dct(lista_blocchi, d, f_dim):
     lista_blocchi_inversa = []
-    
+
+    #Applico DCT per ogni blocco f:
     for f in lista_blocchi:
         c = dct(np.transpose(dct(np.transpose(f), norm='ortho')), norm='ortho')
-    
+
+        #Taglio delle frequenze c_kl con k+l>d
         for k in range(0, c.shape[0]):
             for l in range(0, c.shape[1]):
                 if k + l >= d:
                     c[k, l] = 0
-                    
+
+        #Applico IDCT per ogni blocco c:
         ff = idct(np.transpose(idct(np.transpose(c), norm='ortho')), norm='ortho')
 
+        #Arrotondamento di ff allintero piu' vicino:
         for i in range(0, ff.shape[0]):
             for j in range(0, ff.shape[1]):
                 ff[i,j] = int(ff[i,j])
 
+                #Se esistono valori negativi, porli uguali a 0:
                 if ff[i, j] < 0:
                     ff[i,j] = 0
+                #Se esistono valori maggiori di 255,
+                #porli uguali a 255:
                 elif ff[i,j] > 255:
                     ff[i,j] = 255
 
         lista_blocchi_inversa.append(ff)
-        
-    
+
+    return lista_blocchi_inversa
+
+
+def ricomponi(img, lista_blocchi_inversa, f):
+    col = lista_blocchi_inversa[0]
+    colonne = []
+
     i = 0
     j = 0
     index = 1
-    
-    col = lista_blocchi_inversa[0]
-
-    global colonne
-    colonne = []
-    
-    while (i + f_dim< img.shape[0]):
-        while(j + f_dim < img.shape[1]):
-            j = j + f_dim
-            if j + f_dim < img.shape[1]:
+    while (i + f < img.shape[0]):
+        while(j + f  < img.shape[1]):
+            j = j + f
+            if j + f < img.shape[1]:
                 col = np.hstack((col, lista_blocchi_inversa[index]))
                 index = index + 1
 
-        i = i + f_dim
+        i = i + f
         j = 0
         colonne.append(col)
         if index < len(lista_blocchi_inversa):
@@ -96,13 +89,14 @@ def applica_dct(img, d, f_dim):
 
     for i in range(1, len(colonne)):
         img_compressa = np.vstack((img_compressa, colonne[i]))
-        
+
     img_compressa = img_compressa.astype(np.uint8)
-    cv2.imwrite('montagna_gay.jpg', img_compressa)
-    
-    
-    #f.savefig("bmp_vs_jpg.pdf", bbox_inches='tight')
-    
+    cv2.imwrite('montagna_compressa.jpg', img_compressa)
+
+    return img_compressa
+
+#Plot dell'immagine originale affiancata a quella compressa:
+def plot(img, img_compressa):
     root = Tk()
     root.wm_title("Embedding in Tk")
     
@@ -135,23 +129,39 @@ def applica_dct(img, d, f_dim):
     canvas.get_tk_widget().pack(side=TOP, fill=BOTH, expand=1)
     
     mainloop()
-    
-    
+
+
+######################################
+#     Funzione principale per        #
+#   l'implementazione del software.  #
+#Valori passati come parametri: F e d#
+######################################
 def main_function(f, d) :
     if d < 0 or d > 2 * f - 2:
         showerror("Errore", "d dev'essere compresa fra 0 e 2F-2")
 
-    global img
+    #caricamento dell'immagine scelta dall'utente:
     img = cv2.imread(os.path.basename("montagna.bmp"), 0)
 
-    img_suddivisa = suddividi(img, f)
+    #Suddivisione dell'immagine in blocchi F x F:
+    lista_blocchi = suddividi(img, f)
 
-    applica_dct(img_suddivisa, d, f)
+    #Operazioni sui blocchi:
+    lista_blocchi_inversa = applica_dct(lista_blocchi, d, f)
+    
+    #Composizione dei nuovi blocchi:
+    img_compressa = ricomponi(img, lista_blocchi_inversa, f)
+
+    #Stampa dell'immagine originale e di quella compressa:
+    plot(img, img_compressa)
 
 
 
-######## open file ##########
-
+#######################
+# Metodo implementato #
+#    per l'apertura   #
+#     del file .bmp   #
+#######################
 def open_file(root, btn2):
     file = askopenfile(mode ='r', filetypes =[('Immagine bmp - toni di grigio', '*.bmp')])
     w = Label(root, text="File caricato: " + os.path.basename(file.name), justify=CENTER)
@@ -159,10 +169,13 @@ def open_file(root, btn2):
     global file_path
     file_path = file.name
     btn2.configure(state=NORMAL)
-    
 
-######## main funct #########
 
+##############################
+#    Semplice interfaccia    #
+#per scegliere dal filesystem#
+#      un’immagine .bmp      #
+##############################
 def main():
     root = Tk(className='Scegli file')
     root.geometry('350x250')
@@ -177,12 +190,14 @@ def main():
 
     btn.pack(side = TOP, pady = 10)
 
+    #Scelta da parte del'utente del valore F, macro-blocchi:
     var_F = StringVar(root)
     var_F.set("4")
     spin_F = Spinbox(root, from_=0, to=100, width=5,textvariable = var_F)
 
     spin_F.pack()
 
+    #Scelta da parte dell'utente di d, valore intero compreso tra 0 e 2F-2:
     var_d = StringVar(root)
     var_d.set("3")
     spin_d = Spinbox(root, from_=0, to=100, width=5, textvariable = var_d)
@@ -193,8 +208,6 @@ def main():
     btn2 = Button(root, text ='Avvia', command = lambda:main_function(int(spin_F.get()), int(spin_d.get()), ))
 
     btn2.pack(side = TOP, pady = 10)
-
-
 
     mainloop()
 
